1d2089955393e845f7e0d1aeec934f8393c712c4
cn-cy <65844722+cn-cy@users.noreply.github.com>
2020-05-24 17:02:20 +0800

    Update IStudentService.java

--- SpringDemo/src/com/service/IStudentService.java
+++ SpringDemo/src/com/service/IStudentService.java
@@ -1,7 +1,7 @@
 package com.service;
 
 import com.demo.Student;
-
+//aaaaa
 public interface IStudentService {
 public void addStudent(Student student);
 public void deleteStudentByNo(String string);
