771536109ddaa62ba95602ae8e0dc9e6b246160c
cn-cy <65844722+cn-cy@users.noreply.github.com>
2020-05-24 16:18:08 +0800

    Update LogAfter.java
    abcd

--- SpringDemo/src/com/aop/LogAfter.java
+++ SpringDemo/src/com/aop/LogAfter.java
@@ -4,12 +4,13 @@ import java.lang.reflect.Method;
 
 import org.springframework.aop.AfterReturningAdvice;
 //123456789
+//abcd
 public class LogAfter implements AfterReturningAdvice {
 
 	@Override
 	public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
 		// TODO Auto-generated method stub
-		System.out.println("后置通知-----");
+		System.out.println("潞贸脰脙脥篓脰陋-----");
 	}
 
 }
